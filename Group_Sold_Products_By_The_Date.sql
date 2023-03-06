08b7d800bec18c904d4693d9febd26c9c335be9c
RAGHAVJIMZN <raghavdiwakar17@gmail.com>
2023-03-06 08:22:10 +0530

    .

---
+++ Group_Sold_Products_By_The_Date.sql
@@ -0,0 +1,33 @@
+/*
+
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+
+
+*/
+
+SELECT sell_date,
+       COUNT(DISTINCT(product)) AS num_sold, 
+       GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date ASC;
